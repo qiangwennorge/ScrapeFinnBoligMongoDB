80466cce2a3df11b760663e5b322a14197fdebda
qiangwennorge <qiang.wen.norge@gmail.com>
2017-03-29 20:35:26 +0200

    Save query result in MongoDB successfully

--- ScrapeFinnBoligMongoDB.py
+++ ScrapeFinnBoligMongoDB.py
@@ -9,17 +9,22 @@ from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
 import re
+import pymongo
 
-def GetLinkOfEachRealestate(LinkList, soup):
+# Get Bolig information in each page
+
+def GetInfoOfEachRealestate(BoligDoc,soup):
     for ParentalTag in soup.find_all('div','unit flex align-items-stretch result-item'):
-        OneLinkOfEachRealestate = ParentalTag.contents[1].get('href')
-        #print OneLinkOfEachRealestate
-        LinkList.append(OneLinkOfEachRealestate)
-    return LinkList
+        BoligFinnCode = ParentalTag.contents[1].get('id')
+        BoligTitle = ParentalTag.contents[1].find_all('h3')[0].string
+        EachBoligDoc = {'boligfinncode': BoligFinnCode,
+                        'boligtitle':BoligTitle}  
+        BoligDoc.append(EachBoligDoc)
+    return BoligDoc
 
+# Start from first page
 
-clientmongo = MongoClient(host = "localhost", port=27017)
-databasehandler = clientmongo["FinnBoligDB"]
+BoligDoc = []
 
 BoligUrl = "https://www.finn.no/realestate/homes/search.html?filters="
 BoligResponse = requests.get(BoligUrl)
@@ -27,13 +32,15 @@ BoligContent = BoligResponse.content
 
 BoligSoup = BeautifulSoup(BoligContent,"html.parser")
 
+BoligDoc = GetInfoOfEachRealestate(BoligDoc,BoligSoup) 
+
+# Find the next page number
+
 InitialTagsForNextPage = BoligSoup.find_all('div','t4 centerify r-margin')[0]
 SecondPage = InitialTagsForNextPage.find_all('a',class_='pam')[0].get('href')
-
 NextPageNum = int(re.findall(r'page=(\d+)', SecondPage)[0])
 
-LinkList = []
-FirstLinkList = GetLinkOfEachRealestate(LinkList, BoligSoup)
+# Go to the rest pages
 
 for PageNum in range(NextPageNum,3):
     NextPage = re.sub(r'page=(\d+)', 'page='+str(PageNum), SecondPage)
@@ -41,13 +48,13 @@ for PageNum in range(NextPageNum,3):
     NextResponse = requests.get(NextUrl)
     NextHtml = NextResponse.content
     print PageNum
-    NextSoup = BeautifulSoup(NextHtml,"html.parser")    
-    NextLinkList = GetLinkOfEachRealestate(FirstLinkList, NextSoup)
+    NextSoup = BeautifulSoup(NextHtml,"html.parser")
+    TotalBoligDoc = GetInfoOfEachRealestate(BoligDoc,NextSoup)    
 
+# Save collected bolig information to MongoDB
+clientmongo = MongoClient(host = "localhost", port=27017)
+databasehandler = clientmongo["FinnBoligDB"]
+for EachBoligDoc in BoligDoc:
+    databasehandler.boligcollection.insert(EachBoligDoc,safe=True)
 
-
-
-
-
-#SecondLinkList = GetLinkOfEachRealestate(LinkList, InitialSoup)
 
